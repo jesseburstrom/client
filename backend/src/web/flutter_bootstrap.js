19225575c1b2b7e2b989e4c8c62bb4fe4b70b509
Jesse Burström <jesse@prevas.se>
2025-04-11 13:09:38 +0200

    fixes

--- backend/src/web/flutter_bootstrap.js
+++ backend/src/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3511378868"
+    serviceWorkerVersion: "2973644421"
   }
 });
